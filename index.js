57ddfa4433a34a364bd555f0d1f3d7db3db7e081
Fahimchowdhury23 <fahimchowdhury877@gmail.com>
2025-06-17 12:50:47 +0600

    database connected

--- index.js
+++ index.js
@@ -2,5 +2,37 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster23.fbyecqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster23`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const booksCollection = client.db("booksDB").collection("books");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("books are cooking");
+});
+
+app.listen(port, () => {
+  console.log("app is listening", port);
+});
